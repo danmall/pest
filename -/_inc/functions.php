937e3eaf92b45c88756fca9099be743d332fa5db
Dan Mall <dan@danielmall.com>
2013-08-28 03:57:56 -0400

    Markup complete with PHP logic for time

--- -/_inc/functions.php
+++ -/_inc/functions.php
@@ -1,9 +1,19 @@
 <?php
 
-/*function segmentURL($url){    
-    $segments = explode("/", $url);    
-}
-segmentURL($_SERVER['REQUEST_URI']);*/
+	$currentTime = date('g:i:s a');
 
+	$lastMinute = intval(date('i'))-1;
+
+	if(date('a') == 'am'){
+		$amPmAbbr = 'Ante Meridien';
+	}else{
+		$amPmAbbr = 'Post Meridien';
+	}
+	
+
+
+	function buildLastMinute($multiple, $min, $amPmAbbr){
+		echo date('g:') . $min . ':' . ($multiple * 10) . ' <abbr title="' . $amPmAbbr .'">' . date('a') . '</abbr>';
+	}
 
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/functions.php"); echo "\n"; ?>
 <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/doctype.php"); echo "\n"; ?>
 <head>    
-    <title></title>
+    <title>Progressively Enhanced Stock Table</title>
     <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/meta.php"); echo "\n"; ?>        
 
     <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/cssReference.php"); echo "\n"; ?>        
@@ -11,5 +11,45 @@
     <!--[if lt IE 7]>
         <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
     <![endif]-->
+
+    <h1>Progressively Enhanced Stock Table</h1>
+
+    <section class="last-updated">
+    	<h1>Last updated <?php echo $currentTime; ?>. <strong class="static-page-warning"><a href="../">Refresh</a> for updates.</strong></h1>
+    </section><!-- .last-updated -->
+
+    <div class="stock-table-wrapper">
+
+	    <table class="stock-table">
+	    	<thead>
+	    		<th scope="col">Time</th>
+	    		<th scope="col">Stock Price</th>
+	    	</thead>
+	    	<tbody>
+		    	<tr>
+		    		<th scope="row"><?php buildLastMinute(1, $lastMinute, $amPmAbbr); ?></th>
+		    		<td class="share-price">$10</td>
+		    	</tr>
+		    	<tr>
+		    		<th scope="row"><?php buildLastMinute(2, $lastMinute, $amPmAbbr); ?></th>
+		    		<td class="share-price">$10</td>
+		    	</tr>
+		    	<tr>
+		    		<th scope="row"><?php buildLastMinute(3, $lastMinute, $amPmAbbr); ?></th>
+		    		<td class="share-price">$10</td>
+		    	</tr>
+		    	<tr>
+		    		<th scope="row"><?php buildLastMinute(4, $lastMinute, $amPmAbbr); ?></th>
+		    		<td class="share-price">$10</td>
+		    	</tr>
+		    	<tr>
+		    		<th scope="row"><?php buildLastMinute(5, $lastMinute, $amPmAbbr); ?></th>
+		    		<td class="share-price">$10</td>
+		    	</tr>		    	
+		    </tbody>
+	    </table><!-- .stock-table -->
+
+	</div><!-- .stock-table-wrapper -->
+
 
 <?php require($_SERVER["DOCUMENT_ROOT"]."/-/_inc/close.php"); echo "\n"; ?>    
