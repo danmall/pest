73390eb8e8852054578e13b6b4fe12356096053f
Dan Mall <dan@danielmall.com>
2013-08-28 06:20:37 -0400

    Moving Site object out of document.ready

--- -/j/main.js
+++ -/j/main.js
@@ -11,65 +11,62 @@ http://superfriend.ly/
 *************************************************
 */
 
+var Site = function(){
 
-$(document).ready(function() { 
+    this.init = function(){
+        convertTableToGraph();
+        this.updateTime();
+    }
     
-    var Site = function(){
+    var convertTableToGraph = function(){
 
-        this.init = function(){
-            convertTableToGraph();
-            this.updateTime();
-        }
-        
-        var convertTableToGraph = function(){
+        $('.stock-table').addClass('graph-table');
+        $('.table-headers').addClass('offscreen');
+        $('.share-price').addClass('phark');
 
-            $('.stock-table').addClass('graph-table');
-            $('.table-headers').addClass('offscreen');
-            $('.share-price').addClass('phark');
+        $('.static-page-message').remove();
 
-            $('.static-page-message').remove();
-
-            // add position to dots
-            $('.share-price-num').each(function(n){
-                var _topPos = $(this).text();
-                $(this).parents('.share-price').css({
-                    'left': (n*20)+15 + '%',
-                    'top': _topPos + 'px'
-                });
+        // add position to dots
+        $('.share-price-num').each(function(n){
+            var _topPos = $(this).text();
+            $(this).parents('.share-price').css({
+                'left': (n*20)+15 + '%',
+                'top': _topPos + 'px'
             });
-            
-        }
-
-        this.updateTime = function(){
-            setInterval(function(){
-                var _currentTime = new Date();
-                var _hours = _currentTime.getHours();
-                if(_hours <= 12) {
-                    var _amPm = 'am';                
-                }else{
-                    var _amPm = 'pm';
-                    _hours -= 12;
-                }
-                var _seconds = _currentTime.getSeconds();
-                if(_seconds < 10){
-                    _seconds = '0' + _seconds;
-                }
-
-                $('.current-time').html(_hours + ':' + _currentTime.getMinutes() + ':' + _seconds + ' ' + _amPm);
-            }, 1000);
-        }
-
-    };
-    
-    
-    /*-------------------------------------------    
-        Initial Actions
-    -------------------------------------------*/
+        });
         
-    var pest = new Site();
-    pest.init();
+    }
 
-    
+    this.updateTime = function(){
+        setInterval(function(){
+            var _currentTime = new Date();
+            var _hours = _currentTime.getHours();
+            if(_hours <= 12) {
+                var _amPm = 'am';                
+            }else{
+                var _amPm = 'pm';
+                _hours -= 12;
+            }
+            var _seconds = _currentTime.getSeconds();
+            if(_seconds < 10){
+                _seconds = '0' + _seconds;
+            }
+
+            $('.current-time').html(_hours + ':' + _currentTime.getMinutes() + ':' + _seconds + ' ' + _amPm);
+        }, 1000);
+    }
+
+};
+
+
+
+/*-------------------------------------------    
+    Initial Actions
+-------------------------------------------*/
+
+$(document).ready(function() {         
+    var pest = new Site();
+    pest.init();    
     
 });
 
